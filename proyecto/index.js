adc811534146a9dbb8983c42bc60976f90d26656
cbazaga <bazagacarlos@gmail.com>
2022-02-06 18:53:58 +0100

    Último commit

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = "3000";
+const port = process.env.PORT || 3977;
 const urlMongoAtlas = "mongodb+srv://admin:admin@prueba.isywf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(urlMongoAtlas, (err, res)=>{
